e8280c6e639bd03f44e38e3c9c7758f5ba989a28
yhpp <angchen2@outlook.com>
2023-01-12 11:33:23 +0800

    88. Merge Sorted Array & 704. Binary Search

---
+++ LeetCSharp/Array/704_Binary Search.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Array
+{
+    //Input: nums = [-1,0,3,5,9,12], target = 9
+    //Output: 4
+    //Explanation: 9 exists in nums and its index is 4
+    public class _704_Binary_Search
+    {
+        public int Search1(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int Search2(int[] nums, int target)
+        {
+            int l = 0;
+            int r = nums.Length - 1;
+            while (l <= r)
+            {
+                int midIndex = l + (r - l) / 2;
+                int midValue = nums[midIndex];
+                if (midValue == target)
+                {
+                    return midIndex;
+                }
+                else if (midValue < target)
+                {
+                    l = midIndex + 1;
+                }
+                else
+                {
+                    r = midIndex - 1;
+
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCSharp/Array/88_Merge Sorted Array.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Array
+{
+    //Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    //Output: [1,2,2,3,5,6]
+    public class _88_Merge_Sorted_Array
+    {
+        public void Merge1(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+
+            System.Array.Sort(nums1);
+        }
+
+        public void Merge2(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int[] temp = new int[m + n];
+            while (i < m && j < n)
+            {
+                if (nums1[i] < nums2[j])
+                {
+                    temp[k] = nums1[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    temp[k] = nums2[j];
+                    j++;
+                    k++;
+                }
+            }
+
+            while (i < m)
+            {
+                temp[k] = nums1[i];
+                i++;
+                k++;
+            }
+
+            while (j < n)
+            {
+                temp[k] = nums2[j];
+                j++;
+                k++;
+            }
+
+            for (int index = 0; index < m + n; index++)
+            {
+                nums1[index] = temp[index];
+            }
+        }
+    }
+}
