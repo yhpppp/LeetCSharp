657849d1077956f789580c4978a904a5e6e3db1a
yhpp <angchen2@outlook.com>
2023-02-07 10:47:52 +0800

    92_Reverse Linked List II

---
+++ LeetCSharp/Solution/92_Reverse Linked List II.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: head = [1,2,3,4,5], left = 2, right = 4
+    //Output: [1,4,3,2,5]
+    public class _92_Reverse_Linked_List_II
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (left == right) { return head; }
+
+            ListNode dummyHead = new ListNode(-1, head);
+
+            ListNode firstLastNode = dummyHead;
+            for (int i = 0; i < left - 1; i++)
+            {
+                firstLastNode = firstLastNode.next;
+            }
+            ListNode secondLastNode = dummyHead;
+            for (int i = 0; i < right; i++)
+            {
+                secondLastNode = secondLastNode.next;
+            }
+
+            ListNode secondHeadNode = firstLastNode.next;
+            firstLastNode.next = null;
+            ListNode thirdHeadNode = secondLastNode.next;
+            secondLastNode.next = null;
+
+
+            ListNode pre = null;
+            ListNode cur = secondHeadNode;
+
+            while (cur != null)
+            {
+                var next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+
+
+            ListNode oldSecondHeadNode = secondHeadNode;
+            secondHeadNode = secondLastNode;
+            secondLastNode = oldSecondHeadNode;
+
+            firstLastNode.next = secondHeadNode;
+            secondLastNode.next = thirdHeadNode;
+
+
+            return dummyHead.next;
+        }
+    }
+}
