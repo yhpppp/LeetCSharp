7d4d8327e51927a4cd99af4055dd94afe72ee9f1
yhpp <angchen2@outlook.com>
2023-02-09 09:34:43 +0800

    234_Palindrome Linked List

---
+++ LeetCSharp/Solution/234_Palindrome Linked List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: head = [1,2,2,1]
+    //Output: true
+    public class _234_Palindrome_Linked_List
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            List<int> list = new List<int>();
+
+            ListNode cur = head;
+            while (cur != null)
+            {
+                list.Add(cur.val);
+                cur = cur.next;
+            }
+
+            int headIndex = 0;
+            int lastIndex = list.Count - 1;
+
+            while (headIndex < lastIndex)
+            {
+                if (list[headIndex] != list[lastIndex])
+                {
+                    return false;
+                }
+
+                headIndex++;
+                lastIndex--;
+            }
+
+            return true;
+        }
+    }
+}
