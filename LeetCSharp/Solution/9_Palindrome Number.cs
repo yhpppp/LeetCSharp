d787eb70a949cb0b38a2f66755621de8957dc9fd
yhpp <angchen2@outlook.com>
2023-02-13 15:00:32 +0800

    9_Palindrome Number

---
+++ LeetCSharp/Solution/9_Palindrome Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: x = 121
+    //Output: true
+    //Explanation: 121 reads as 121 from left to right and from right to left.
+    public class _9_Palindrome_Number
+    {
+        public bool IsPalindrome(int x)
+        {
+            string temp = x.ToString();
+            int i = 0;
+            int j = temp.Length - 1;
+
+            while (i < j)
+            {
+                if (temp[i] != temp[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+
+            }
+
+            return true;
+        }
+    }
+}
