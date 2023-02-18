6dd266fe08eacfacdad3e746d47e77a28374a980
yhpp <angchen2@outlook.com>
2023-02-18 10:34:15 +0800

    145_Binary Tree Postorder Traversal && 94_Binary Tree Inorder Traversal

---
+++ LeetCSharp/Solution/145_Binary Tree Postorder Traversal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [1,null,2,3]
+    //Output: [3,2,1]
+    public class _145_Binary_Tree_Postorder_Traversal
+    {
+        List<int> list = new List<int>();
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            if (root == null) { return list; }
+
+            PostorderTraversal(root.left);
+            PostorderTraversal(root.right);
+            list.Add(root.val);
+
+            return list;
+        }
+
+        public IList<int> PostorderTraversal2(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            if (root == null) { return list; }
+
+            Stack<Command> stacks = new();
+            stacks.Push(new Command("GO", root));
+
+            while (stacks.Count > 0)
+            {
+                var command = stacks.Pop();
+                if (command.name.Equals("DO"))
+                {
+                    list.Add(command.node.val);
+                }
+                else
+                {
+                    stacks.Push(new Command("DO", command.node));
+                    if(command.node.right != null)
+                    {
+                        stacks.Push(new Command("GO", command.node.right));
+                    }
+
+                    if(command.node.left != null)
+                    {
+                        stacks.Push(new Command("GO", command.node.left));
+                    }
+                }
+            }
+        }
+        class Command
+        {
+            public string name; // GO DO
+            public TreeNode node;
+
+            public Command(string name, TreeNode node)
+            {
+                this.name = name;
+                this.node = node;
+            }
+        }
+    }
+}
---
+++ LeetCSharp/Solution/94_Binary Tree Inorder Traversal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [1,null,2,3]
+    //Output: [1,3,2]
+    public class _94_Binary_Tree_Inorder_Traversal
+    {
+        List<int> list = new List<int>();
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+            {
+                return list;
+            }
+
+            InorderTraversal(root.left);
+            list.Add(root.val);
+            InorderTraversal(root.right);
+
+            return list;
+        }
+
+        public IList<int> InorderTraversal2(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            if (root == null) { return list; }
+
+            Stack<Command> stacks = new Stack<Command>();
+            stacks.Push(new Command("GO", root));
+
+            while (stacks.Count > 0)
+            {
+                Command command = stacks.Pop();
+                if (command.name.Equals("DO"))
+                {
+                    list.Add(command.TreeNode.val);
+                }
+                else
+                {
+                    if (command.TreeNode.right != null)
+                    {
+                        stacks.Push(new Command("GO", command.TreeNode.right));
+                    }
+
+                    stacks.Push(new Command("DO", command.TreeNode));
+
+                    if (command.TreeNode.left != null)
+                    {
+                        stacks.Push(new Command("GO", command.TreeNode.left));
+                    }
+                }
+            }
+
+            return list;
+        }
+        class Command
+        {
+            public string name; // DO GO
+            public TreeNode TreeNode;
+
+            public Command(string name, TreeNode treeNode)
+            {
+                this.name = name;
+                TreeNode = treeNode;
+            }
+        }
+
+    }
+}
