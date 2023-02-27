4150d55e990bbb683232639aecf04850d41795ec
yhpp <angchen2@outlook.com>
2023-02-27 14:49:07 +0800

    102_Binary Tree Level Order Traversal

---
+++ LeetCSharp/Solution/102_Binary Tree Level Order Traversal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [3,9,20,null,null,15,7]
+    //Output: [[3],[9,20],[15,7]]
+    public class _102_Binary_Tree_Level_Order_Traversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if(root == null) { return result; }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                List<int> list = new();
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    list.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                result.Add(list);
+            }
+
+            return result;
+        }
+    }
+}
