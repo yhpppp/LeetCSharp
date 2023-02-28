5a443cdd50a1644d1fda22e55242468d38ac386d
yhpp <angchen2@outlook.com>
2023-02-28 13:46:53 +0800

    103_Binary Tree Zigzag Level Order Traversal &&  107_Binary Tree Level Order Traversal II

---
+++ LeetCSharp/Solution/103_Binary Tree Zigzag Level Order Traversal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [3,9,20,null,null,15,7]
+    //Output: [[3],[20,9],[15,7]]
+    public class _103_Binary_Tree_Zigzag_Level_Order_Traversal
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            bool isLeft = true;
+
+            while (queue.Count > 0)
+            {
+                List<int> list = new();
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    list.Add(node.val);
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                if (isLeft)
+                {
+                    result.Add(list);
+                }
+                else
+                {
+                    list.Reverse();
+                    result.Add(list);
+                }
+                isLeft = !isLeft;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCSharp/Solution/107_Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [3,9,20,null,null,15,7]
+    //Output: [[15,7],[9,20],[3]]
+    public class _107_Binary_Tree_Level_Order_Traversal_II
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            List<IList<int>> levelOrder = new List<IList<int>>();
+            if (root == null) { return levelOrder; }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                IList<int> list = new List<int>();
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    list.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                levelOrder.Add(list);
+            }
+
+            levelOrder.Reverse();
+            return levelOrder;
+        }
+    }
+}
