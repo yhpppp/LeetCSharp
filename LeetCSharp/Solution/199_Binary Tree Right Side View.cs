d0192cc3b32308586c9dc22871c637516452f6d2
yhpp <angchen2@outlook.com>
2023-03-01 13:55:11 +0800

    199_Binary Tree Right Side View

---
+++ LeetCSharp/Solution/199_Binary Tree Right Side View.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [1,2,3,null,5,null,4]
+    //Output: [1,3,4]
+    public class _199_Binary_Tree_Right_Side_View
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    if (i == size - 1)
+                    {
+                        result.Add(node.val);
+                    }
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
