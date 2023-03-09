0aa21cc2b03e55a8afc01cd66a3789138c3818e6
yhpp <angchen2@outlook.com>
2023-03-09 10:15:02 +0800

    226_Invert Binary Tree && 112_Path Sum

---
+++ LeetCSharp/Solution/112_Path Sum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+    //Output: true
+    //Explanation: The root-to-leaf path with the target sum is shown.
+    public class _112_Path_Sum
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) { return false; }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            Queue<int> count = new Queue<int>();
+            count.Enqueue(root.val);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                var sum = count.Dequeue();
+                // get to leaf node
+                if (node.left == null && node.right == null)
+                {
+                    if (sum == targetSum)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                        count.Enqueue(sum + node.left.val);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                        count.Enqueue(sum + node.right.val);
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCSharp/Solution/226_Invert Binary Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [4,2,7,1,3,6,9]
+    //Output: [4,7,2,9,6,3,1]
+    public class _226_Invert_Binary_Tree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                var temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            return root;
+        }
+    }
+}
