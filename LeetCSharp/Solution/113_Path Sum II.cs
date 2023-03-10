a8804b7eefca226d71d242ef120ca276d7926721
yhpp <angchen2@outlook.com>
2023-03-10 10:47:27 +0800

    257_Binary Tree Paths && 113_Path Sum II

---
+++ LeetCSharp/Solution/113_Path Sum II.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //    Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+    //Output: [[5,4,11,2],[5,8,4,5]]
+    //Explanation: There are two paths whose sum equals targetSum:
+    //5 + 4 + 11 + 2 = 22
+    //5 + 8 + 4 + 5 = 22
+    public class _113_Path_Sum_II
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            Queue<TreeNode> nodesQueue = new Queue<TreeNode>();
+            nodesQueue.Enqueue(root);
+            Queue<int> sumQueue = new Queue<int>();
+            sumQueue.Enqueue(root.val);
+            Queue<string> strQueue = new Queue<string>();
+            strQueue.Enqueue(root.val.ToString());
+
+            while (nodesQueue.Count > 0)
+            {
+                TreeNode node = nodesQueue.Dequeue();
+                int sum = sumQueue.Dequeue();
+                string str = strQueue.Dequeue();
+
+                if (node.left == null && node.right == null)
+                {
+                    if (sum == targetSum)
+                    {
+                        var strArr = str.Split(',');
+                        var intArr = System.Array.ConvertAll(strArr, int.Parse);
+                        result.Add(intArr);
+                    }
+                }
+
+                if (node.left != null)
+                {
+                    nodesQueue.Enqueue(node.left);
+
+                    sumQueue.Enqueue(sum + node.left.val);
+
+                    StringBuilder sb = new(str);
+                    sb.Append(',').Append(node.left.val);
+                    strQueue.Enqueue(sb.ToString());
+                }
+
+                if (node.right != null)
+                {
+                    nodesQueue.Enqueue(node.right);
+
+                    sumQueue.Enqueue(sum + node.right.val);
+
+                    StringBuilder sb = new(str);
+                    sb.Append(',').Append(node.right.val);
+                    strQueue.Enqueue(sb.ToString());
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCSharp/Solution/257_Binary Tree Paths.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _257_Binary_Tree_Paths
+    {
+        //Input: root = [1,2,3,null,5]
+        //Output: ["1->2->5","1->3"]
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            List<string> paths = new List<string>();
+            if (root == null)
+            {
+                return paths;
+            }
+
+            Queue<string> pathsQueue = new Queue<string>();
+            Queue<TreeNode> treeNodes = new Queue<TreeNode>();
+            treeNodes.Enqueue(root);
+            pathsQueue.Enqueue(root.val.ToString());
+            while (treeNodes.Count > 0)
+            {
+                var node = treeNodes.Dequeue();
+                var path = pathsQueue.Dequeue();
+
+                if (node.left == null && node.right == null)
+                {
+                    paths.Add(path);
+                }
+
+                if (node.left != null)
+                {
+                    treeNodes.Enqueue(node.left);
+
+                    StringBuilder sb = new(path);
+                    sb.Append("->").Append(node.left.val);
+                    pathsQueue.Enqueue(sb.ToString());
+                }
+
+                if (node.right != null)
+                {
+                    treeNodes.Enqueue(node.right);
+
+                    StringBuilder sb = new(path);
+                    sb.Append("->").Append(node.right.val);
+                    pathsQueue.Enqueue(sb.ToString());
+                }
+            }
+
+            return paths;
+        }
+    }
+}
