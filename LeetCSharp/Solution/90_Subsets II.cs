cb280c2d334588355c2ca25beccf1b32317c410d
yhpp <angchen2@outlook.com>
2023-03-15 10:13:10 +0800

    90_Subsets II

---
+++ LeetCSharp/Solution/90_Subsets II.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: nums = [1,2,2]
+    //Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+    public class _90_Subsets_II
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            System.Array.Sort(nums);
+            SubsetsRecursion(nums, 0);
+            return res;
+        }
+
+        List<IList<int>> res = new();
+        List<int> list = new();
+        private void SubsetsRecursion(int[] nums, int start)
+        {
+            res.Add(new List<int>(list));
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i > start && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                list.Add(nums[i]);
+                SubsetsRecursion(nums, i + 1);
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
