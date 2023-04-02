d26732267e5bd4ede9edb6f52b04f61b32a63b4a
yhpp <angchen2@outlook.com>
2023-04-02 22:10:30 +0800

    58_Length of Last Word

---
+++ LeetCSharp/Solution/28_Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: haystack = "sadbutsad", needle = "sad"
+    //Output: 0
+    //Explanation: "sad" occurs at index 0 and 6.
+    //The first occurrence is at index 0, so we return 0.
+    public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String
+    {
+        public int StrStr(string haystack, string needle)
+        {
+  
+        }
+    }
+}
