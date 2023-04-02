cd6c781eabd6750412b8519eeaa6292875dde226
yhpp <angchen2@outlook.com>
2023-04-02 22:10:47 +0800

    58_Length of Last Word

--- LeetCSharp/Solution/58_Length of Last Word.cs
+++ LeetCSharp/Solution/58_Length of Last Word.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace LeetCSharp.Solution
 {
+    //Input: s = "Hello World"
+    //Output: 5
+    //Explanation: The last word is "World" with length 5.
     public class _58_Length_of_Last_Word
     {
+        public int LengthOfLastWord(string s)
+        {
+            int index = s.Length - 1;
+            while (index >= 0 && s[index] == ' ')
+            {
+                index--;
+            }
+            int len = 0;
+            while (index >= 0 && s[index] != ' ')
+            {
+                len++;
+                index--;
+            }
+            return len;
+        }
     }
 }
