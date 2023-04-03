65d4569990cc73fdcc13fc1d58997489296a95c2
yhpp <angchen2@outlook.com>
2023-04-03 10:52:38 +0800

    66_Plus One

--- LeetCSharp/Solution/28_Find the Index of the First Occurrence in a String.cs
+++ LeetCSharp/Solution/28_Find the Index of the First Occurrence in a String.cs
@@ -12,9 +12,9 @@ namespace LeetCSharp.Solution
     //The first occurrence is at index 0, so we return 0.
     public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String
     {
-        public int StrStr(string haystack, string needle)
-        {
+        //public int StrStr(string haystack, string needle)
+        //{
   
-        }
+        //}
     }
 }
---
+++ LeetCSharp/Solution/66_Plus One.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: digits = [9]
+    //Output: [1,0]
+    //Explanation: The array represents the integer 9.
+    //Incrementing by one gives 9 + 1 = 10.
+    //Thus, the result should be[1, 0].
+    public class _66_Plus_One
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> result = new List<int>();
+            int carry = 1;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int item = digits[i] + carry;
+                carry = item == 10 ? 1 : 0;
+                result.Insert(0, item % 10);
+            }
+
+            if (carry == 1)
+            {
+                result.Insert(0, 1);
+            }
+            return result.ToArray();
+        }
+    }
+}
