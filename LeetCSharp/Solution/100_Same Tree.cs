cd73b0649c5902ca32381d8e97bae2aef1969a78
yhpp <angchen2@outlook.com>
2023-04-04 10:43:40 +0800

    100_Same Tree

---
+++ LeetCSharp/Solution/100_Same Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: p = [1,2], q = [1,null,2]
+    //Output: false
+    public class _100_Same_Tree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            if (p == null || q == null)
+            {
+                return false;
+            }
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
