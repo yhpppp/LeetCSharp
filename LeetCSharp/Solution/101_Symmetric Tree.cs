7ca6da47fa45e776fedcbe95ec66bda8b5a81947
yhpp <angchen2@outlook.com>
2023-04-05 10:31:16 +0800

    101_Symmetric Tree

---
+++ LeetCSharp/Solution/101_Symmetric Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: root = [1,2,2,3,4,4,3]
+    //Output: true
+    public class _101_Symmetric_Tree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return IsMirror(root.left, root.right);
+        }
+
+        private bool IsMirror(TreeNode leftNode, TreeNode rightNode)
+        {
+            if (leftNode == null && rightNode == null)
+            {
+                return true;
+            }
+            if (leftNode == null || rightNode == null)
+            {
+                return false;
+            }
+            if (leftNode.val != rightNode.val)
+            {
+                return false;
+            }
+
+            return IsMirror(leftNode.left, rightNode.right) && IsMirror(leftNode.right, rightNode.left);
+        }
+    }
+}
