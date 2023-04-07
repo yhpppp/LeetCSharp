59c9aabf145bdae3696650f7940d7fd587343527
yhpp <angchen2@outlook.com>
2023-04-07 12:17:11 +0800

    _119_Pascal_s_Triangle_II && _136_Single_Number

---
+++ LeetCSharp/Solution/119_Pascal's Triangle II.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: rowIndex = 3
+    //Output: [1,3,3,1]
+    public class _119_Pascal_s_Triangle_II
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var arr = new int[rowIndex + 1];
+            arr[0] = 1;
+            if (rowIndex == 0)
+            {
+                return arr;
+            }
+
+            for (int i = 1; i <= rowIndex; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    arr[j] = arr[j - 1] + arr[j];
+                }
+            }
+            return arr;
+        }
+    }
+}
---
+++ LeetCSharp/Solution/136_Single Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: nums = [2,2,1]
+    //Output: 1
+    public class _136_Single_Number
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (var item in nums)
+            {
+                result = result ^ item;
+            }
+            return result;
+        }
+    }
+}
