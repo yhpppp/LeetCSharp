bbe8376a208f1fbfd7f0096f83353196d0464259
yhpp <angchen2@outlook.com>
2023-04-19 22:48:50 +0800

    171_Excel Sheet Column Number

---
+++ LeetCSharp/Solution/171_Excel Sheet Column Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _171_Excel_Sheet_Column_Number
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int num = 0;
+            foreach (char ch in columnTitle)
+            {
+                num *= 26;
+                num += ch - 'A' + 1;
+            }
+            return num;
+        }
+    }
+}
