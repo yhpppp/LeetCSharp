41cfb0dc78d8f0d0eb9575d48fe8770ef67a32a2
yhpp <angchen2@outlook.com>
2023-04-26 21:50:37 +0800

    168_Excel Sheet Column Title

---
+++ LeetCSharp/Solution/168_Excel Sheet Column Title.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    //Input: columnNumber = 28
+    //Output: "AB"
+    public class _168_Excel_Sheet_Column_Title
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder sb = new StringBuilder();
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+
+                int d = columnNumber % 26;
+                sb.Insert(0, chars[d]);
+
+                columnNumber /= 26;
+            }
+            return sb.ToString();
+        }
+    }
+}
