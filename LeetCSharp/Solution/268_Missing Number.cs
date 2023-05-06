692732f3b526a2e81cd8638dc3bf20adbeddf785
yhpp <angchen2@outlook.com>
2023-05-06 22:32:10 +0800

    _268_Missing_Number

---
+++ LeetCSharp/Solution/268_Missing Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _268_Missing_Number
+    {
+        //Input: nums = [3,0,1]
+        //Output: 2
+        //Explanation: n = 3 since there are 3 numbers, so all numbers are in the range[0, 3]. 2 is the missing number in the range since it does not appear in nums.
+        public int MissingNumber(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result +=  i + 1 - nums[i];
+            }
+            return result;
+        }
+    }
+}
