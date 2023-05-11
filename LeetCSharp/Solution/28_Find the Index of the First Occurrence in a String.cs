bd97a23b43f2652ccfcaa0e521bc9477bd9b1afe
yhpp <angchen2@outlook.com>
2023-05-11 11:38:41 +0800

    28_Find the Index of the First Occurrence in a String

--- LeetCSharp/Solution/28_Find the Index of the First Occurrence in a String.cs
+++ LeetCSharp/Solution/28_Find the Index of the First Occurrence in a String.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 
 namespace LeetCSharp.Solution
 {
-    //Input: haystack = "sadbutsad", needle = "sad"
-    //Output: 0
+    //Input: haystack = "mississippi", needle = "issip"
+    //Output: 4
     //Explanation: "sad" occurs at index 0 and 6.
     //The first occurrence is at index 0, so we return 0.
     public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String
     {
-        //public int StrStr(string haystack, string needle)
-        //{
-  
-        //}
+        public int StrStr(string haystack, string needle)
+        {
+            int index = 0;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[index])
+                {
+                    index++;
+                    if (needle.Length == index)
+                    {
+                        return i + 1 - needle.Length;
+                    }
+                }
+                else
+                {
+                    i = i - index;
+                    index = 0;
+                }
+            }
+
+            return -1;
+        }
     }
 }
