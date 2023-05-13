743ff6304ccf2574ae5519628fdc817cd3d4757a
yhpp <angchen2@outlook.com>
2023-05-13 20:31:35 +0800

    448_Find All Numbers Disappeared in an Array

---
+++ LeetCSharp/Solution/448_Find All Numbers Disappeared in an Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _448_Find_All_Numbers_Disappeared_in_an_Array
+    {
+        //Input: nums = [4,3,2,7,8,2,3,1]
+        //Output: [5,6]
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0)
+                {
+                    nums[index] *= -1;
+                }
+            }
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    result.Add(i + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
