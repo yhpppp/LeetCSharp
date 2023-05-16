9b3e762cad8eeb9c24a82042148165d2fa8280d6
yhpp <angchen2@outlook.com>
2023-05-16 10:51:22 +0800

    383_Ransom Note & 389_Find the Difference

---
+++ LeetCSharp/Solution/383_Ransom Note.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _383_Ransom_Note
+    {
+        //Input: ransomNote = "a", magazine = "b"
+        //Output: false
+        //Input: ransomNote = "aab", magazine = "baa"
+        //Output: true
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (var item in magazine)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic.Add(item, 1);
+                }
+            }
+            foreach (var item in ransomNote)
+            {
+                if (dic.ContainsKey(item) && dic[item] > 0)
+                {
+                    dic[item]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCSharp/Solution/389_Find the Difference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _389_Find_the_Difference
+    {
+        //Input: s = "abcd", t = "abcde"
+        //Output: "e"
+        //Explanation: 'e' is the letter that was added.
+        public char FindTheDifference(string s, string t)
+        {
+            int letter = 0;
+            foreach (var item in t)
+            {
+                letter += item;
+            }
+            foreach (var item in s)
+            {
+                letter -= item;
+            }
+            return (char)letter;
+        }
+
+        public char FindTheDifference2(string s, string t)
+        {
+            List<char> list = new List<char>(t);
+            foreach (var item in s)
+            {
+                list.Remove(item);
+            }
+            return list[0];
+        }
+
+    }
+}
