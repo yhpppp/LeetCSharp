5d23aba01aef43f7057169a90b81f1aeae83bfbc
yhpp <angchen2@outlook.com>
2023-05-17 21:56:20 +0800

    520_Detect Capital

---
+++ LeetCSharp/Solution/520_Detect Capital.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCSharp.Solution
+{
+    public class _520_Detect_Capital
+    {
+        //Input: word = "USA"
+        //Output: true
+        //Input: word = "FlaG"
+        //Output: false
+        //Input: word = "g"
+        //Output: true
+        public bool DetectCapitalUse(string word)
+        {
+            int upper = 0;
+            foreach (char c in word)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    upper++;
+                }
+            }
+            if (upper == 1 && word[0] <= 'Z')
+            {
+                return true;
+            }
+            return upper == word.Length || upper == 0;
+        }
+    }
+}
